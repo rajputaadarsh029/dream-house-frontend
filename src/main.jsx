bf34401c00c99d817ac04269effc0dd299c4819e
Aadarsh Rajput <rajputaadarsh029@gmail.com>
2025-11-09 11:04:09 +0530

    Fixed script module types for Render

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import "./index.css";
 
 import Home from "src/pages/Home.jsx";
 import Editor from "src/pages/Editor.jsx";
-import Dashboard from "src/pages/Dashboard.jsx";
+import Dashboard from "./pages/dashboard.jsx";
+
 import Login from "src/pages/Login.jsx";
 import Register from "src/pages/Register.jsx";
 
